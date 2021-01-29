71de7ae1dc7ea407fed2f04dacbb1741a141ab40
alvinkhanh <alvinrataja63@gmail.com>
2021-01-30 01:03:55 +0700

    new feature

--- jadwal/get_mengajar.php
+++ jadwal/get_mengajar.php
@@ -3,16 +3,31 @@
 	$jurusan = $_POST['jurusan'];
 
 	echo "<option value=''>Pilih dosen & matakuliah</option>";
-
-	$query = "SELECT dosen.nama, mengajar.id_mengajar, matkul.nama_matkul 
+	
+	$query = "SELECT DISTINCT mengajar.kode_jurusan, matkul.nama_matkul, dosen.nama
 	FROM mengajar, matkul, dosen
-	WHERE kode_jurusan=? 
-	ORDER BY mengajar.kode_dosen ASC";
+	WHERE kode_jurusan=?
+	ORDER BY mengajar.kode_jurusan ASC";
+	
+	// $query = "SELECT mengajar.kode_jurusan, matkul.id_matkul, dosen.id
+	// FROM mengajar
+	// INNER JOIN (SELECT DISTINCT kode_jurusan, nama_matkul, nama 
+	// FROM dosen) dosen
+	// ON mengajar.kode_jurusan = ?
+	// ORDER BY kode_jurusan
+	// ";
+
+	// $query = "SELECT DISTINCT mengajar.kode_jurusan, matkul.nama_matkul, dosen.nama
+	// FROM mengajar, matkul, dosen
+	// WHERE kode_jurusan=?
+	// ORDER BY mengajar.kode_jurusan ASC";
 	$dewan1 = $conn->prepare($query);
-	$dewan1->bind_param("i", $jurusan);
+	$dewan1->bind_param("s", $jurusan);
 	$dewan1->execute();
 	$res1 = $dewan1->get_result();
-	while ($row = $res1->fetch_assoc()) {
-		echo "<option value='" . $row['id_mengajar'] . "'>" . $row['nama'] .  "</option>";
+	echo var_dump($dewan1);
+	while ($row = $res1->fetch_assoc()) {		
+		echo "<option value='" . $row['id_mengajar'] . "'>" . $row['nama'] . "--" . $row['nama_matkul'] . "</option>";
 	}
+
 ?>
